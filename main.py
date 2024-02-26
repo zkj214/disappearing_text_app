9581049d2d30e35929cca81b45516cd70595b28c
Ziebhar <pythonexercise214@gmail.com>
2024-02-27 00:33:54 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,58 @@
+from tkinter import *
+from tkinter import filedialog,messagebox
+
+instruction="If you don't press any key for 5 seconds, the text you have typed will disappear"
+
+window=Tk()
+window.config(width=500,height=500,bg="white",padx=20,pady=20)
+window.title("Disappearing Text")
+
+header=Label(text="Dangerous Writing App",font=("Calibri",20,"bold"),bg="white",fg="black")
+header.grid(column=0,row=0)
+
+text=Label(text=instruction,font=("Helvetica",12,"normal"),bg="white",fg="black")
+text.grid(column=0,row=1)
+
+text_input=Text(width=50,bg="white",fg="black",borderwidth=0)
+text_input.grid(column=0,row=2,padx=20,pady=20)
+text_input.focus()
+
+
+def reset_app():
+    text_input.delete(1.0,END)
+
+
+timer = window.after(5000, reset_app)
+
+
+def type_txt(event):
+    global timer,user_txt
+
+    key=event.char
+
+    window.after_cancel(timer)
+    timer = window.after(5000, reset_app)
+
+
+def save_txt():
+    global timer
+    window.after_cancel(timer)
+    user_txt=text_input.get(1.0,"end-1c")
+
+    file_path=filedialog.asksaveasfilename(defaultextension=".txt", filetypes=[("All Files","*.*"),("Word File","*.docx"),("Text File","*.txt")])
+    file_name=file_path.split('/')[-1]
+
+    with open(f"{file_path}",'w') as file:
+        file.write(f"{user_txt}")
+
+    messagebox.showinfo(title="Success!",message=f"{file_name} has been successfully saved.")
+
+
+save_btn=Button(text="Save",padx=10,pady=10, font=("arial",12,"normal"),command=save_txt)
+save_btn.grid(column=0,row=3)
+
+
+window.bind("<Key>",type_txt)
+
+
+window.mainloop()
